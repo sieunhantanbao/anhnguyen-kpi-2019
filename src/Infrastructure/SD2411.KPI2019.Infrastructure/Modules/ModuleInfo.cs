78fea17a3e7b63b7ed71091dcbbe61b02af8771c
HARVEYNASH\anhnguyens <ext.anh.nguyensieu@achilles.com>
2019-07-23 22:43:54 +0700

    Commit code - add modules

---
+++ src/Infrastructure/SD2411.KPI2019.Infrastructure/Modules/ModuleInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SD2411.KPI2019.Infrastructure.Modules
+{
+    public class ModuleInfo
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Version Version { get; set; }
+    }
+}
